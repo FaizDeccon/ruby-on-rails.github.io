ac69335f2ea265e53c62a151d7681af633715cdc
Faiz Ali <faiz.ali@devsinc.com>
2021-07-16 19:18:26 +0500

    Completed Web Blog App
    Contains security and validation checks as well

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
-# frozen_string_literal: true
-
 Rails.application.routes.draw do
-  root 'articles#index'
+  root "articles#index"
   resources :articles do
     resources :comments
   end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
